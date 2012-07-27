144912798ac659a33c1b28c91b2302980f7085e7
Taylor Mark Dignan <dignan.taylor@gmail.com>
2012-07-27 14:56:00 -0400

    Initial commit

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -18,7 +18,7 @@ class UsersControllerTest < ActionController::TestCase
 
   test "should create user" do
     assert_difference('User.count') do
-      post :create, user: { email: @user.email, name: @user.name }
+      post :create, user: { name: @user.name, name: @user.name }
     end
 
     assert_redirected_to user_path(assigns(:user))
@@ -35,7 +35,7 @@ class UsersControllerTest < ActionController::TestCase
   end
 
   test "should update user" do
-    put :update, id: @user, user: { email: @user.email, name: @user.name }
+    put :update, id: @user, user: { name: @user.name, name: @user.name }
     assert_redirected_to user_path(assigns(:user))
   end
 
